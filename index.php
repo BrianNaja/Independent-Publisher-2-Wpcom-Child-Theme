53d97355d5e744e010975f79df710b3f4fbd5ad0
Brian Najafabadi <naja.bp@gmail.com>
2017-09-22 13:28:38 -0400

    Check if user loged in with redirect
    Check if user loged in with redirect
    
    https://wordpress.stackexchange.com/questions/227115/redirect-to-login-page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
+<?php	
+
 /**
  * The main template file.
  *
@@ -10,16 +11,21 @@
  *
  * @package Independent_Publisher_2
  */
+	
+//  PHP using logic "! NOT" and "|| OR" operator to check if current user is logged in	 
+ 
+if ( !current_user_can('editor') || !current_user_can('administrator') || !current_user_can('member') ) {
+    wp_redirect( wp_login_url() );
+    exit;
+}
+
+//  PHP using logic "! NOT" and "|| OR" operator to check if current user is logged in END
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
-			<!-- PHP using logic "OR" operator to check if current user is logged in	 -->					
-		<?php if ( current_user_can('editor') || current_user_can('administrator') || current_user_can('member') ) {?>
-
-		
+	
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
@@ -54,13 +60,6 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-		
-		<?php } else { echo "<strong>You have failed me for the last time! <br>You must LOGIN to the dark side.</strong>
-			<br><img src=../wp-content/uploads/2017/09/darth_vader_rogue_one.jpg <br> <p>Add some styling to the login form below and delete Darth Vader when done</p> <br>";
-			 wp_login_form();
-		}?>
-
-<!-- PHP using logic "OR" operator to check if current user is logged in END	 -->	
 		
 	</div><!-- #primary -->
 <?php get_sidebar(); ?>
