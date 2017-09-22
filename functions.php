b67feffc5942f895fc7492acae10341bf86eed1d
Brian Najafabadi <naja.bp@gmail.com>
2017-09-22 14:05:59 -0400

    Change login page logo
    Change login page logo
    
    login logo added to images folder
    
    https://codex.wordpress.org/Customizing_the_Login_Form

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php  
+
+// Change the Login Logo
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+		height:65px;
+		width:320px;
+		background-size: 80px 80px;
+		background-repeat: no-repeat;
+        	padding-bottom: 20px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+// Change the link values so the logo links to your WordPress site
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Pittsburgh Firefighters';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
